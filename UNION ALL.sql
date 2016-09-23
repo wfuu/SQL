5e4d8462d1395e5cc5888d987765ab68ef003e04
Wen Fu <wen_fu@outlook.com>
2016-09-23 11:30:25 -0400

    Upload

--- UNION ALL.sql
+++ UNION ALL.sql
@@ -1,4 +1,6 @@
-# Scenario for "concatenating" two tables: There is a column of makers and a column of count of items they make. First, group both columns by maker, then create an additional row (with two entries) at the bottom of the table with "Total_count" and the total count of items by all makers
+# Scenario for "concatenating" two tables: There is a column of makers and a column of count of items they make
+# First, group both columns by maker, then create an additional row (with two entries) at the bottom of the table 
+# with "Total_count" and the total count of items by all makers
 # Could be hacky: Need to filter both selections if not including all rows from the table
 
 SELECT maker, COUNT(1) as 'Count_of_items'
